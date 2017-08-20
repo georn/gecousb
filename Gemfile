e06a72e95a08a3a18791a975abac81a7d723a116
georn <mrog0@outlook.com>
2017-08-20 22:08:30 +0100

    staticPages controller with home route as root

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ group :development, :test do
 end
 
 group :test do
+  gem 'rails-controller-testing'
   gem 'faker', '~> 1.1.2'
   gem 'simplecov'
   gem 'simplecov-console'
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
+  describe "GET home" do
+    it "responds with 200" do
+      get :home
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the #home view" do
+      get :home
+      expect(response).to render_template :home
+    end
+  end
 end
