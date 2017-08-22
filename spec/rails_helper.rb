c05b2e821b7c5dabc39171123455a77a36fce879
georn <mrog0@outlook.com>
2017-08-22 03:59:52 +0100

    add user sign up, log in, log out

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'clearance/rspec'
+require "clearance/test_unit"
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
