04ebbbcee97bb5191ebb532eb09c965bb7b12916
georn <mrog0@outlook.com>
2017-08-23 21:33:45 +0100

    generating post model and migrating database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822010801) do
+ActiveRecord::Schema.define(version: 20170823202639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "course"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema.define(version: 20170822010801) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "posts", "users"
 end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :post do
+    title "MyString"
+    course "MyString"
+    content "MyText"
+    user nil
+  end
+end
