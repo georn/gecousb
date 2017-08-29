59cbd941b5d32e8ddcb2b929ce5670bd919d38f1
georn <mrog0@outlook.com>
2017-08-29 21:48:41 +0100

    constraining actions in the show method for a post and ordering the post in the index method by id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :require_login, only: [:new, :create, :edit, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(:id)
   end
 
   def show
