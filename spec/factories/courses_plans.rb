309691072fe3b53c868f82f3825e8e728e80bda9
georn <mrog0@outlook.com>
2017-08-30 00:03:32 +0100

    setting up courses_plan factory to work in a trimester

--- spec/factories/courses_plans.rb
+++ spec/factories/courses_plans.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :courses_plan do
-    start_date "2017-08-29"
-    end_date "2017-08-29"
-    user nil
+    start_date { Date.today }
+    end_date { 3.months.from_now.to_date }
+
+    association(:user)
   end
 end
