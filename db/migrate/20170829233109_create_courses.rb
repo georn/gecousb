ae667e7b3d340688fb3bbdb7d987f219bb18febb
georn <mrog0@outlook.com>
2017-08-30 01:32:10 +0100

    eliminating career attribute from courses

--- db/migrate/20170829233109_create_courses.rb
+++ db/migrate/20170829233109_create_courses.rb
@@ -3,7 +3,6 @@ class CreateCourses < ActiveRecord::Migration[5.1]
     create_table :courses do |t|
       t.string :code, limit: 6
       t.string :department, limit: 65
-      # t.string :careers, limit: 4
       t.references :courses_plan, foreign_key: true
       t.references :post, foreign_key: true
 
