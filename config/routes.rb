96e4da8f830c9e87257f616756276591d2035dc7
georn <mrog0@outlook.com>
2017-09-18 23:05:10 +0100

    Enable the routes to the course controller in the application * Adding access to the course index page through the navbar * Enabling the routes in the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/sobre-nosotros', to: 'static_pages#about'
-  get '/oauth2callback', to: 'static_pages#oauth2callback'
+  get '/oauth2callback', to: 'courses#oauth2callback'
 
   resources :posts
+  resources :courses, only: [:index, :show]
 end
